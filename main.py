a6aaa4b7c8c0caf21fa06f3146b1ba9cbfeebdae
Perla Godinez <contactoperlagodinez@gmail.com>
2025-04-02 11:35:29 -0600

    post method works!

--- main.py
+++ main.py
@@ -14,8 +14,13 @@ def get_user(id):
     if query:
         user["query"] = query
     return jsonify(user), 200
-
 #/users/123?query=query_test
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    data["status"] = "user created"
+    return jsonify(data), 201
 
 
 if __name__ == '__main__':
