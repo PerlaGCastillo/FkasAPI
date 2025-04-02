74c19c8aabf3e13125d7a27cc0b4b51291f4da86
Perla Godinez <contactoperlagodinez@gmail.com>
2025-04-02 11:07:05 -0600

    get method works

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from flask import Flask, jsonify, request
 
 app =Flask(__name__)
+
+@app.route("/")
+def root():
+    return "root"
+
+
+@app.route("/users/<id>")
+def get_user(id):
+    user= {"id":id, "name": "test", "telefono": "999-666-333"}
+    query = request.args.get('query')
+    if query:
+        user["query"] = query
+    return jsonify(user), 200
+
+#/users/123?query=query_test
+
+
 if __name__ == '__main__':
     app.run(debug=True)
